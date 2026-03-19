935a7007891cc44583d503fcb1365a802250fd9d
Alicia Bentley <abentley@vm-cprk-emcrzdm01.ncep.noaa.gov>
2026-03-19 19:39:04 +0000

    Added style .css files and updated copy_directories.sh

--- util/copy_directories.sh
+++ util/copy_directories.sh
@@ -2,8 +2,8 @@
 
 # --- Configuration ---
 # Replace these with your actual paths
-SOURCE="/home/people/emc/www/htdocs/users/verification_hold/global/aigefs/expr/"
-DESTINATION="/home/people/emc/www/htdocs/users/verification_restricted/global/aigefs/dev/"
+SOURCE="/home/people/emc/www/htdocs/users/verification/global/gfs/para/"
+DESTINATION="/home/people/emc/www/htdocs/users/verification/global/gfs/prod/"
 
 # --- Execution ---
 # Check if source exists
